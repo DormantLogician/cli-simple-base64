0c537687d4bd4fccad8334b3817c8b9d2c8bd530
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-02-01 17:49:51 -0500

    Change versioning system used by project to something more simple.
    For example, version 1, version 2, etc.

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,10 @@
 from conans import ConanFile
 
 class ConfigConan(ConanFile):
-    name = "csb64"
-    version = "0.0.1"
+    name = "cli-sb64"
+    version = "v1"
     license = "MIT"
-    description = """Command line interface for SimpleBase64 encoder and decoder."""
+    description = """Command line interface for sb64 base 64 encoder/decoder."""
     settings = "os", "compiler", "build_type", "arch"
     tool_requires = "boost/1.81.0"
     generators = "cmake_find_package"
