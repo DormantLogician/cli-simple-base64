cd06eacb0a5ff1d3a17b03303d31561269f4e5cf
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-02-01 18:27:49 -0500

    Make concat_bits in sb64 private and remove tests.
    Other tests should provide enough coverage, as the unit is being used in other tested units under Letter.

--- target/lib/sb64/inc/sb64/Letter.h
+++ target/lib/sb64/inc/sb64/Letter.h
@@ -13,12 +13,12 @@ namespace sb64
             static std::vector<bool> truncate_bits(const std::vector<bool>& input);
             static std::vector<Letter> by_bits(const std::vector<Letter>& bit_groups,
                                                const long long group_max_size);
+            static std::vector<bool> concat_bits(const std::vector<Letter>& input);
 
         public:
             static const unsigned char max_size;
 
             static std::vector<unsigned char> as_uchars(const std::vector<Letter>& input);
-            static std::vector<bool> concat_bits(const std::vector<Letter>& input);
             static std::vector<Letter> by_eight_bits(const std::vector<Letter>& bit_groups);
             static std::vector<Letter> by_six_bits(const std::vector<Letter>& bit_groups);
 
