355e5c95c7a679669c5c9ba2919989bcae220f5e
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-02-01 23:12:27 -0500

    Fix some Lintian errors/warnings for generated deb files that are applicable.

--- util/build/cmake/setup/cpack.cmake
+++ util/build/cmake/setup/cpack.cmake
@@ -1,8 +1,15 @@
-set(CPACK_PACKAGE_CONTACT "Stephen Aaron Hannon")
+set(CPACK_PACKAGE_CONTACT "Stephen Aaron Hannon <hannonstephen19@gmail.com>")
+set(CPACK_STRIP_FILES YES) # Strip debug symbols from binary files.
+set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS # Make sure directory permissions are set properly.
+    OWNER_READ OWNER_WRITE OWNER_EXECUTE
+    GROUP_READ GROUP_EXECUTE
+    WORLD_READ WORLD_EXECUTE
+)
 
 set(CPACK_SOURCE_GENERATOR "ZIP")
 set(CPACK_SOURCE_IGNORE_FILES /built;/.git)
 
 set(CPACK_GENERATOR "DEB")
-set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT) # Use Debian naming conventions for binary files.
 set(CPACK_DEBIAN_PACKAGE_SECTION utils)
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES) # Make better dependency list in deb file.
