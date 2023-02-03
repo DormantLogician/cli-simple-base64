b6a7ef221bdfd29d7b1089a39411138acc2f1787
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-02-03 13:29:22 -0500

    Add doc comments to 'csb64_logic' headers for future reference.
    I don't think it is necessary to document all the 'flag' value getter functions.

--- target/lib/csb64_logic/inc/csb64_logic/run_csb64.h
+++ target/lib/csb64_logic/inc/csb64_logic/run_csb64.h
@@ -6,6 +6,21 @@
 
 namespace csb64_logic
 {
+    /*
+     * Runner for the application (any input/output functionality is injected
+     * in).
+     *
+     * @param user_args Input to command line (mirrors 'argv' array).
+     * @param read_character Outputs a stream of characters from some arbitrary
+     * location one by one.
+     * @param write_character Writes a character to some arbitrary location.
+     * @param at_end_of_stream Returns true if we are at end of some stream -
+     * false otherwise.
+     * @param print_error Is called to print error messages to some arbirary
+     * location.
+     * @param print_error Is called to print help info for program to some
+     * arbirary location.
+     */
     int run_csb64(const std::vector<std::string>& user_args,
                   const std::function<int()>& read_character,
                   const std::function<void(const unsigned char character)> write_character,
