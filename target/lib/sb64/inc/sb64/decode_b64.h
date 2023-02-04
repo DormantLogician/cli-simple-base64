c5ffc5b87dfa3d0f311e1aee63bde8814d07f71e
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-02-03 19:48:41 -0500

    Add doc comments for sb64 interface (headers).

--- target/lib/sb64/inc/sb64/decode_b64.h
+++ target/lib/sb64/inc/sb64/decode_b64.h
@@ -7,6 +7,14 @@
 
 namespace sb64
 {
+    /*
+     * Decodes base 64 encoded data (with padding, and using regular alphabet).
+     *
+     * @param reader Yields characters from some arbitrary location until they
+     * are depleted.
+     * @param writer Writes decoded data words at a time to some arbitrary
+     * location.
+     */
     void decode_b64(const std::function<std::optional<unsigned char>()>& reader,
                     const std::function<bool(const std::vector<unsigned char>& output_text)>& writer);
 };
