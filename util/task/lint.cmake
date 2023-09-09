cd98b16b8ea9d1b43d528e922a5e10cb9aca1440
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-09-09 15:10:22 -0400

    Migrate to Conan 2.0 - bringing project up to date with current state of CMake template.
    This commit adds support for checksums and better integration with
    Conan. It also introduces a new script 'package.cmake' for easier
    building.

---
+++ util/task/lint.cmake
@@ -0,0 +1,42 @@
+find_program(CONAN_EXE NAMES "conan" DOC "Path to Conan package manager tool.")
+find_program(CTEST_EXE NAMES "ctest" DOC "Path to CTest test runner.")
+
+if (NOT CONAN_EXE)
+    message(SEND_ERROR 
+        "Conan package manager executable not found on system - this is required to get dependencies for the project."
+    )
+endif()
+
+if (NOT CTEST_EXE)
+    message(SEND_ERROR 
+        "CTest test runner executable not found on system - this is required to run the project's tests."
+    )
+endif()
+
+if (NOT CONAN_EXE OR NOT CTEST_EXE)
+    message(FATAL_ERROR 
+        "Not all programs required to run script are available on system - please check which programs are missing above."
+    )
+endif()
+
+set(BUILT_DIR built/Debug)
+set(DEBUG_PRESET debug)
+set(TEST_PRESET test)
+
+message(STATUS "Run Conan...")
+file(MAKE_DIRECTORY ${BUILT_DIR})
+execute_process(COMMAND ${CONAN_EXE} install . --build=missing -s build_type=Debug COMMAND_ERROR_IS_FATAL ANY)
+
+message(STATUS "Configure CMake project...")
+execute_process(COMMAND ${CMAKE_COMMAND} --preset ${TEST_PRESET} COMMAND_ERROR_IS_FATAL ANY)
+
+message(STATUS "Build and test with sanitizers...")
+execute_process(COMMAND ${CMAKE_COMMAND} --build --preset ${TEST_PRESET} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND ${CTEST_EXE} --preset ${TEST_PRESET} COMMAND_ERROR_IS_FATAL ANY)
+
+message(STATUS "Configure CMake project...")
+execute_process(COMMAND ${CMAKE_COMMAND} --preset ${DEBUG_PRESET} COMMAND_ERROR_IS_FATAL ANY)
+
+message(STATUS "Build and test with memory checker...")
+execute_process(COMMAND ${CMAKE_COMMAND} --build --preset ${DEBUG_PRESET} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND ${CTEST_EXE} -M Experimental -T MemCheck WORKING_DIRECTORY ${BUILT_DIR})
